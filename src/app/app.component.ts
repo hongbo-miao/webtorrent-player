a4dab7db1cab8062d80ea1d99fcd0e5f73e63b24
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-12-03 12:07:46 -0500

    add webtorrent stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as WebTorrent from 'webtorrent';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+    video {
+      width: 100%;
+    }
+  `],
+  template: `
+    WebTorrent Player
+    <video #output>
+    </video>
+  `
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('output') private output: ElementRef;
+
+  ngAfterViewInit() {
+    // const torrentId = 'https://webtorrent.io/torrents/sintel.torrent';
+    const torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4';
+
+    const client = new WebTorrent();
+    console.log('client', client);
+
+    client.add(torrentId, torrent => {
+      console.log('torrent.files', torrent.files);
+
+      torrent.files[0].renderTo(this.output.nativeElement);
+    });
+  }
 }
