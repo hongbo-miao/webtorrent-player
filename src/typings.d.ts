a486cb9b804ed4490d6cfc5f9fee4f33751720b8
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-12-04 22:43:17 -0500

    chore: add own typings to remove warning

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,8 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+interface Document {
+  fullscreen: any;
+  mozFullScreen: any;
+  mozCancelFullScreen: any;
+}
