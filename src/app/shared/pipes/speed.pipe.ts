26e88bae6d30bff7a645cc8af0aabded805d9ceb
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-12-05 18:27:06 -0500

    ux: speed pipe

--- src/app/shared/pipes/speed.pipe.ts
+++ src/app/shared/pipes/speed.pipe.ts
@@ -5,11 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SpeedPipe implements PipeTransform {
   transform(num: number): string {
-    if (num === undefined) return '';
+    if (num === undefined) return '0 B/s';
 
     const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-    if (num < 1) return `${num} B`;
+    if (num < 1) return `${num} B/s`;
 
     const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
     const newNum = Number((num / Math.pow(1000, exponent)).toFixed(1));
